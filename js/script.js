a6f1fbd9069d978b5d1b602484c515be7a9b1004
JKoenen99 <jap.koenen@hotmail.nl>
2019-11-04 12:57:32 +0100

    added interaction to the animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+var button = document.getElementById('button');
+
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('lottie'), // Required
   path: 'js/data.json', // Required
   renderer: 'svg', // Required
   loop: true, // Optional
-  autoplay: true, // Optional
+  autoplay: false, // Optional
   name: "Lost in space", // Name for future reference. Optional.
 })
 
+
+button.addEventListener("mouseenter", function() {
+animation.play();
+});
+
+button.addEventListener("mouseleave", function() {
+animation.stop();
+});
